828751234ddfec287b2cb099bf8631193326abc8
Yuriy <yourgen.edu@mail.ru>
2020-04-25 18:23:37 +0300

    добавлен контроллер задачи

---
+++ src/controllers/task.js
@@ -0,0 +1,12 @@
+import Task from "../components/task.js";
+import TaskEdit from "../components/task-edit.js";
+import {render, replace, RenderPosition} from "../utils/render.js";
+
+export default class TaskController {
+  constructor(container) {
+  }
+
+  render(task) {
+      
+  }
+}
